7b4c9cfe7e5411d1923e048aac610f32f05d3746
ranjeetprasad <ranjeet.prasad@paytm.com>
2025-11-23 18:42:54 +0530

    inmplemented Singleton Design Pattern using Lazy Way

--- Singleton/src/Car.java
+++ Singleton/src/Car.java
@@ -1,5 +1,14 @@
 public class Car {
 
     //Lazy way of creating Singleton Object
-
+    // constructor should be private so that external Client can not be call
+    private Car() {
+    }
+    private static Car car;
+    public static Car getCar() {
+        if (car == null) {
+            car = new Car();
+        }
+        return car;
+    }
 }
--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("------------Singleton Desing Pattern using Lazy Way---------");
+        Car car1 = Car.getCar();
+        Car car2 = Car.getCar();
+        if (car1.hashCode() == car2.hashCode()) {
+            System.out.println("Cars are the same! So it follow Singleton Design Pattern using Lazy Way");
+        } else {
+            System.out.println("Cars are not the same!");
+        }
 
-        for (int i = 1; i <= 5; i++)
-            System.out.println("i = " + i);
     }
 }
