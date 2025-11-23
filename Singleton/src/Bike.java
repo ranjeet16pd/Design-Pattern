6747c20662ed7638647cb90129286ea3f12393b8
ranjeetprasad <ranjeet.prasad@paytm.com>
2025-11-23 19:08:47 +0530

    added eger way to implement Singleton Pattern

---
+++ Singleton/src/Bike.java
@@ -0,0 +1,12 @@
+public class Bike {
+
+    // Eger Way to implement Singleton Design Pattern
+    private static Bike bike = new Bike();      //--> Ye class Load hote hi sirf ek bar hi create hoga
+
+
+    // getBike ye sirf ek reference ko return krta h
+    public static Bike getBike() {
+        return bike;
+    }
+
+}
--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,13 +1,24 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("------------Singleton Desing Pattern using Lazy Way---------");
+        System.out.println("------------Singleton Design Pattern using Lazy Way---------");
         Car car1 = Car.getCar();
         Car car2 = Car.getCar();
         if (car1.hashCode() == car2.hashCode()) {
             System.out.println("Cars are the same! So it follow Singleton Design Pattern using Lazy Way");
         } else {
             System.out.println("Cars are not the same!");
+        }
+
+
+        System.out.println("------------Singleton Design Pattern using Eger Way---------");
+
+        Bike bike1 = Bike.getBike();
+        Bike bike2 = Bike.getBike();
+        if (bike1.hashCode() == bike2.hashCode()) {
+            System.out.println("Bikes are the same!");
+        } else {
+            System.out.println("Bikes are not the same!");
         }
 
     }
