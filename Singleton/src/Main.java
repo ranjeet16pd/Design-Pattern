e92ec0eba6e5a5c062a7f0d3047e7af3035c55d7
ranjeetprasad <ranjeet.prasad@paytm.com>
2025-12-06 18:04:53 +0530

    singleton break using reflection API

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,6 +1,9 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+       /*
         System.out.println("------------Singleton Design Pattern using Lazy Way---------");
         Car car1 = Car.getCar();
         Car car2 = Car.getCar();
@@ -34,5 +37,22 @@ public class Main {
         Thread thread2 = new Thread(task);
         thread1.start();
         thread2.start();
+
+        */
+
+        /*
+        Ways 3 to break singleton pattern
+         */
+
+        Bus bus1 = Bus.getBus();
+        System.out.println("Car 1 Hash code is " + bus1.hashCode());
+
+        // Now making the new instance of class using reflection API
+        Constructor<Car> constructor = Car.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Car bus2 = constructor.newInstance();
+        System.out.println("Car 2 Hash code is " + bus2.hashCode());
+
+
     }
 }
