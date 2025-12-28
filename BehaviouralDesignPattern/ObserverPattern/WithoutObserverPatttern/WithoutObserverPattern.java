c6d19da7516f3a2bd296082fcc62ecc36f6b8c7d
ranjeetprasad <ranjeet.prasad@paytm.com>
2025-12-28 14:30:29 +0530

    done code with without observer Pattern

---
+++ BehaviouralDesignPattern/ObserverPattern/WithoutObserverPatttern/WithoutObserverPattern.java
@@ -0,0 +1,44 @@
+package WithoutObserverPatttern;
+
+
+class DisplayDevices {
+
+    private float temperature;
+
+    public void showTempreture(float temperature) {
+        System.out.println("Current temperature: " + temperature + "\u00B0");
+    }
+}
+
+class WhetherStation {
+    private float temperature;
+    private final DisplayDevices displayDevices;
+
+
+    WhetherStation(DisplayDevices displayDevices) {
+        this.displayDevices = displayDevices;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+        notifyToDevices();
+    }
+
+    public void notifyToDevices() {
+        displayDevices.showTempreture(temperature);
+    }
+}
+
+
+public class WithoutObserverPattern {
+
+    public static void main(String[] args) {
+        DisplayDevices displayDevices = new DisplayDevices();
+        // very tight coupling code
+        WhetherStation whetherStation = new WhetherStation(displayDevices);
+        whetherStation.setTemperature(25.4f);
+        whetherStation.setTemperature(55.4f);
+    }
+
+
+}
