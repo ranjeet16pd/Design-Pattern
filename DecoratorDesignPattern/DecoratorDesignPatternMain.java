94768e1df75cf6ac174ceeb5e6cb99aa6e115522
ranjeetprasad <ranjeet.prasad@paytm.com>
2026-09-03 06:11:00 +0530

    Done with Decorator Pattern

--- DecoratorDesignPattern/DecoratorDesignPatternMain.java
+++ DecoratorDesignPattern/DecoratorDesignPatternMain.java
@@ -61,4 +61,21 @@ class StartPowerUp extends Decorator {
 
 
 public class DecoratorDesignPatternMain {
+
+    public static void main(String[] args) {
+        Character mario = new Mario();
+        System.out.println("Basic Character: " + mario.getAbilities());
+
+        mario = new HeightUp(mario);
+        System.out.println("After HeightUp: " + mario.getAbilities());
+
+        // Decorate Mario further with a GunPowerUp.
+        mario = new GunPowerUp(mario);
+        System.out.println("After GunPowerUp: " + mario.getAbilities());
+
+        // Finally, add a StarPowerUp decoration.
+        mario = new StartPowerUp(mario);
+        System.out.println("After StarPowerUp: " + mario.getAbilities());
+    }
+
 }
